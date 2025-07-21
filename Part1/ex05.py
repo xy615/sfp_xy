273ad6dee4679c6a91a095951989bbb59721ee4b
xy615 <25068115@imail.sunway.edu.my>
2025-07-21 12:26:46 +0000

    work done for day 1

--- Part1/ex05.py
+++ Part1/ex05.py
@@ -2,5 +2,5 @@ word1 = 'car'
 word2 = 'exceeded'
 text1 = 'speed limit'
 word3 = 'the'
-z = word3 + word1 + word2 + word3 + text1
+z = word3 + " " + word1 + " " + word2+ " " + word3+ " " + text1
 print (z)
